2692c1df30fe95df55ca3f74e8018d8027c31e09
er vimalyadav <er.vimalyadav.viyad@gmail.com>
2024-12-28 15:35:17 +0530

    search bar and button

---
+++ engine/features.py
@@ -0,0 +1,8 @@
+from playsound import playsound
+
+
+
+#play music assistant
+def playAssistantSound():
+    music_dir="www\\assets\\audio\\start_sound.mp3"
+    playsound(music_dir)
--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import os
 
 import eel
 
+from engine.features import *
+
+
 eel.init("www")
+
+playAssistantSound
 
 os.system('start msedge.exe --app="http://localhost:8000/index.html"' )
 
