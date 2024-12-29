3c564a0895ee41111884d139f1ac3c23c6cccc42
er vimalyadav <er.vimalyadav.viyad@gmail.com>
2024-12-29 17:08:55 +0530

    lastupdate 29/12

--- engine/command.py
+++ engine/command.py
@@ -1,13 +1,39 @@
 import pyttsx3
-import 
+import speech_recognition as sr
 
 
 def speak(text):
+        
     engine = pyttsx3.init()
-    voices = engine.getProperty('voices')       #getting details of current voice
-    engine.setProperty('voice', voices[0].id)
-    engine.setProperty("rate",150) 
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id) #changing index changes voices but only 0 and 1 are working here
+    print(voices)
     engine.say(text)
-    engine.runAndwait()
+    engine.runAndWait()
 
-    speak("I love India")
+def takecommand():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('listening....')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print('recognizing')
+        query = r.recognize_google(audio, language='en-in')
+        print(f"You said: {query}")
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+
+text = takecommand()
+
+speak(text)
+
+    
